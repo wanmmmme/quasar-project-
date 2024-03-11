e7cec86f66e17a4a695b533de358a8668b180dff
wanmmmme <metineechaturaphat@gmail.com>
2024-03-11 18:25:02 +0700

    เมธินี อัจฉราภัสร์ 6404101333

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,32 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
+  <h1 class="center">Quasar Form</h1>
+  <q-page class="flex flex-center alineTop">
+    <!-- <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >
+    > -->
+    <From />
   </q-page>
+
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import From from '../components/Form.vue'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: {
+    From
+  }
+
 })
 </script>
+
+<style scoped>
+.center {
+  text-align: center;
+}
+
+</style>
